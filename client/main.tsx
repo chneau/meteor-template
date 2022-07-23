dea1243aff6ad89d4d18571acfd754e4e269387f
Charles Neau <charles63500@gmail.com>
2022-07-23 22:11:15 +0000

    Adding strict mode

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { StrictMode } from "react";
 window.React = React;
 
 import { createRoot } from "react-dom/client";
-import { App } from "/imports/ui/App";
+import { AccountProvider } from "/imports/ui/contexts/AccountContext";
+import { Hello } from "/imports/ui/Hello";
+import { Info } from "/imports/ui/Info";
 
 const container = document.getElementById("react-target");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <AccountProvider>
+      <Hello />
+      <Info />
+    </AccountProvider>
+  </StrictMode>
+);
--- imports/ui/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Hello } from "./Hello";
-import { Info } from "./Info";
-
-export const App = () => (
-  <div>
-    <Hello />
-    <Info />
-  </div>
-);
