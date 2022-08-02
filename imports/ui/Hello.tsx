df9c38fcc44bf66b3dec960ee13620aa4466c3b0
chneau <charles63500@gmail.com>
2022-08-02 19:39:35 +0100

    Cool buttons

--- imports/ui/Hello.tsx
+++ imports/ui/Hello.tsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Users } from "../api/Users";
 import { Authorize } from "./components/Authorize";
+import { AccountContext } from "./contexts/AccountContext";
 
 export const Hello = () => {
   const [counter, setCounter] = useState(0);
   const increment = () => setCounter((prev) => prev + 1);
+  const { user } = useContext(AccountContext);
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
       <Button onClick={increment}>Click Me</Button>
       <p>You've pressed the button {counter} times.</p>
-      <Button onClick={() => Users.logout()}>Logout</Button>
-      <Button onClick={() => Users.loginWithPassword("user", "user")}>Login as user</Button>
-      <Button onClick={() => Users.loginWithPassword("admin", "admin")}>Login as admin</Button>
+      <ButtonGroup>
+        <Button onClick={() => Users.loginWithPassword("user", "user")} disabled={user?.username == "user"}>
+          Login as user
+        </Button>
+        <Button onClick={() => Users.loginWithPassword("admin", "admin")} disabled={user?.username == "admin"}>
+          Login as admin
+        </Button>
+        <Button variant="danger" onClick={() => Users.logout()} disabled={!user}>
+          Logout
+        </Button>
+      </ButtonGroup>
       <Authorize>
         <p>You can see this only if you are logged in.</p>
       </Authorize>
       <Authorize roles={["admin"]}>
         <p>You can see this only if you are logged in and are an admin.</p>
       </Authorize>
-      <Link to="/admin">Try accessing /admin</Link>
+      <div>
+        <Link to="/admin">Try accessing /admin</Link>
+      </div>
     </div>
   );
 };
