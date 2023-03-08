eca90d0e42cf367068499ab3f8f578dbc1afa409
chneau <charles63500@gmail.com>
2023-03-08 15:23:21 +0000

    cool

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { ViteFinal } from "@storybook/builder-vite";
+import type { StorybookConfig } from "@storybook/react-vite";
+
+export default {
+  stories: ["../imports/**/*.mdx", "../imports/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  features: {
+    // interactionsDebugger: true,
+  },
+  core: {
+    disableTelemetry: true,
+  },
+} satisfies StorybookConfig;
+
+export const viteFinal: ViteFinal = async (config, options) => {
+  config.clearScreen = false;
+  return config;
+};
