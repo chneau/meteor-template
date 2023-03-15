bee3239d97003fe11de2329462e3059c132d3822
chneau <charles63500@gmail.com>
2023-03-15 12:48:49 +0000

    fix

--- imports/ui/pages/HomePage.tsx
+++ imports/ui/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import { useTracker } from "meteor/react-meteor-data";
 import { Hello } from "../Hello";
 import { Info } from "../Info";
+import { Links } from "/imports/api/Links";
 
 export const HomePage = () => {
-  const links = useTracker(() => Info.subAll());
+  const links = useTracker(() => Links.subAll());
   return (
     <>
       <Hello />
