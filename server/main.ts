5c996a453fc257385b686dea2e644d4a726202b7
chneau <charles63500@gmail.com>
2024-05-29 09:16:25 +0100

    Refactor CreateLink and Info components to use TypeScript type annotations

--- server/main.ts
+++ server/main.ts
@@ -16,23 +16,25 @@ await Links.upsertAsync({
 	title: "Do the Tutorial",
 	url: "https://www.meteor.com/tutorials/react/creating-an-app",
 });
-Links.upsertAsync({
+await Links.upsertAsync({
 	title: "Follow the Guide",
 	url: "http://guide.meteor.com",
 });
-Links.upsertAsync({ title: "Read the Docs", url: "https://docs.meteor.com" });
-Links.upsertAsync({ title: "Discussions", url: "https://forums.meteor.com" });
+await Links.upsertAsync({
+	title: "Read the Docs",
+	url: "https://docs.meteor.com",
+});
+await Links.upsertAsync({
+	title: "Discussions",
+	url: "https://forums.meteor.com",
+});
 
-const tryIgnore = (fn: () => void) => {
-	try {
-		fn();
-	} catch (e) {}
-};
-
-tryIgnore(() =>
-	Users.createAsync({ username: "admin", password: "admin", roles: ["admin"] }),
-);
-tryIgnore(() => Users.createAsync({ username: "user", password: "user" }));
+await Users.createAsync({
+	username: "admin",
+	password: "admin",
+	roles: ["admin"],
+}).catch(() => 0);
+await Users.createAsync({ username: "user", password: "user" }).catch(() => 0);
 
 // auto publish roles information to the user
 Meteor.publish(null, function () {
